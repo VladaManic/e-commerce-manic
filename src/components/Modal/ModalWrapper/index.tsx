a870c1e6429e0fd8e61816ad3fef9dfea5504d52
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2023-09-08 10:15:31 +0200

    click on overlay closes modal

--- src/components/Modal/ModalWrapper/index.tsx
+++ src/components/Modal/ModalWrapper/index.tsx
@@ -1,4 +1,6 @@
 import ReactDom from 'react-dom';
+import {useContext} from 'react';
+import ModalContext from '../../../context/modal-context';
 
 import { ModalWrap, Overlay, ButtonWrap } from './style';
 
@@ -9,11 +11,17 @@ interface Props {
 }
 
 const ModalWrapper = ({ open, children, onClose }: Props) => {
+	const modalCtx = useContext(ModalContext);
+
 	if (!open) return null;
+
+	const onClickHandler = () => {
+		modalCtx.setIsOpen(false);
+	}
 
 	return ReactDom.createPortal(
 		<>
-			<Overlay />
+			<Overlay onClick={onClickHandler} />
 			<ModalWrap>
 				<ButtonWrap onClick={onClose}>x</ButtonWrap>
 				{children}
