91298e078daaf25663b974be22789b99cdb035e0
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2023-12-08 10:51:35 +0100

    loading context return

--- src/main.tsx
+++ src/main.tsx
@@ -10,16 +10,18 @@ import { ModalContextProvider } from './context/ModalContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<ErrorContextProvider>
-			<ProductsContextProvider>
-				<CartContextProvider>
-					<ModalContextProvider>
-						<BrowserRouter>
-							<App />
-						</BrowserRouter>
-					</ModalContextProvider>
-				</CartContextProvider>
-			</ProductsContextProvider>
-		</ErrorContextProvider>
+		<LoadingContextProvider>
+			<ErrorContextProvider>
+				<ProductsContextProvider>
+					<CartContextProvider>
+						<ModalContextProvider>
+							<BrowserRouter>
+								<App />
+							</BrowserRouter>
+						</ModalContextProvider>
+					</CartContextProvider>
+				</ProductsContextProvider>
+			</ErrorContextProvider>
+		</LoadingContextProvider>
 	</React.StrictMode>
 );
