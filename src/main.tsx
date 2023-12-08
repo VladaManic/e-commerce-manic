34ce9992fbbc6f51276944da5be81154b0908d61
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2023-12-08 10:26:21 +0100

    strict mode return

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorContextProvider } from './context/error-context';
 import { ProductsContextProvider } from './context/products-context';
@@ -7,15 +8,17 @@ import { CartContextProvider } from './context/cart-context';
 import { ModalContextProvider } from './context/modal-context';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ErrorContextProvider>
-    <ProductsContextProvider>
-      <CartContextProvider>
-        <ModalContextProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </ModalContextProvider>
-      </CartContextProvider>
-    </ProductsContextProvider>
-  </ErrorContextProvider>,
-)
+	<React.StrictMode>
+		<ErrorContextProvider>
+			<ProductsContextProvider>
+				<CartContextProvider>
+					<ModalContextProvider>
+						<BrowserRouter>
+							<App />
+						</BrowserRouter>
+					</ModalContextProvider>
+				</CartContextProvider>
+			</ProductsContextProvider>
+		</ErrorContextProvider>
+	</React.StrictMode>
+);
